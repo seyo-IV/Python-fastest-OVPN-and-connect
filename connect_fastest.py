a0d49117d0f26d03c456231734857729f3fa19e7
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2023-03-05 17:18:22 +0100

    Create connect_fastest.py

---
+++ connect_fastest.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+import re
+import speedtest
+
+def find_fastest_vpn_server(vpn_dir):
+    fastest_server = None
+    highest_speed = 0
+    for filename in os.listdir(vpn_dir):
+        if not filename.endswith('.ovpn'):
+            continue
+        filepath = os.path.join(vpn_dir, filename)
+        server_address = extract_server_address_from_ovpn_file(filepath)
+        st = speedtest.Speedtest()
+        st.download_target = server_address
+        speed = st.download() / 1e6  # Convert to Mbps
+        if speed > highest_speed:
+            highest_speed = speed
+            fastest_server = server_address
+    return fastest_server
+
+
+vpn_dir = '/path/to/vpn/files'
+fastest_server = find_fastest_vpn_server(vpn_dir)
