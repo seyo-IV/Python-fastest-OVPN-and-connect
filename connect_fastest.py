cd01384da121b37a25e13ea5820ee80d22df6177
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2023-03-06 09:10:37 +0100

    Update connect_fastest.py

--- connect_fastest.py
+++ connect_fastest.py
@@ -3,30 +3,35 @@ import subprocess
 import re
 import speedtest
 
-def find_fastest_vpn_server(vpn_dir):
+def extract_server_address_from_ovpn_file(filename):
+    with open(filename) as f:
+        content = f.read()
+    server_address = re.findall('remote\s+(\S+)', content)[0]
+    return server_address
+
+
+def find_fastest_vpn_server(vpn_files):
     fastest_server = None
     highest_speed = 0
-    for filename in os.listdir(vpn_dir):
-        if not filename.endswith('.ovpn'):
-            continue
-        filepath = os.path.join(vpn_dir, filename)
-        server_address = extract_server_address_from_ovpn_file(filepath)
+    for vpn_file in vpn_files:
+        server_address = extract_server_address_from_ovpn_file(vpn_file)
         st = speedtest.Speedtest()
         st.download_target = server_address
         speed = st.download() / 1e6  # Convert to Mbps
         if speed > highest_speed:
             highest_speed = speed
-            fastest_server = {
-                'address': server_address,
-                'username': 'your_username',
-                'password': 'your_password'
-            }
+            fastest_server = server_address
     return fastest_server
 
 
-vpn_dir = '/path/to/vpn/files'
-fastest_server = find_fastest_vpn_server(vpn_dir)
+
+
+
+ovpn_directory = '/path/to/ovpn/files'
+ovpn_files = [os.path.join(ovpn_directory, f) for f in os.listdir(ovpn_directory) if f.endswith('.ovpn')]
+
+fastest_server = find_fastest_vpn_server(ovpn_files)
 
 # Connect to fastest server using openvpn command
-command = f'sudo openvpn --config {fastest_server["address"]} --auth-user-pass <(echo "{fastest_server["username"]}\n{fastest_server["password"]}")'
+command = f'sudo openvpn --config {fastest_server}'
 subprocess.run(command, shell=True, check=True)
