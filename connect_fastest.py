89275a19a9978709524447c30690a85998c88891
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2023-03-09 09:53:09 +0100

    Update connect_fastest.py
    Added error handling

--- connect_fastest.py
+++ connect_fastest.py
@@ -4,29 +4,23 @@ import re
 import speedtest
 
 def extract_server_address_from_ovpn_file(filename):
-    print(f"Extracting server address from {filename}")
     with open(filename) as f:
         content = f.read()
     server_address = re.findall('remote\s+(\S+)', content)[0]
-    print(f"Server address: {server_address}")
     return server_address
 
 
 def find_fastest_vpn_server(vpn_files):
-    print("Finding fastest VPN server...")
     fastest_server = None
     highest_speed = 0
     for vpn_file in vpn_files:
-        print(f"Testing server in {vpn_file}")
         server_address = extract_server_address_from_ovpn_file(vpn_file)
         st = speedtest.Speedtest()
         st.download_target = server_address
         speed = st.download() / 1e6  # Convert to Mbps
-        print(f"Download speed: {speed:.2f} Mbps")
         if speed > highest_speed:
             highest_speed = speed
             fastest_server = vpn_file
-    print(f"Fastest server: {fastest_server}")
     return fastest_server
 
 
@@ -34,10 +28,16 @@ ovpn_directory = '/opt/ovpn'
 ovpn_files = [os.path.join(ovpn_directory, f) for f in os.listdir(ovpn_directory) if f.endswith('.ovpn')]
 
 fastest_server_file = find_fastest_vpn_server(ovpn_files)
+if fastest_server_file is None:
+    print("Could not find any VPN files in the directory.")
+    exit(1)
 fastest_server = extract_server_address_from_ovpn_file(fastest_server_file)
 
 # Connect to fastest server using openvpn command
-print(f"Connecting to fastest server: {fastest_server}")
 command = f'sudo openvpn --config {fastest_server_file}'
-result = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
-print(result.stdout.decode())
+try:
+    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(result.stdout.decode())
+except subprocess.CalledProcessError as e:
+    print(f"Error occurred: {e}")
+    print(e.stderr.decode())
